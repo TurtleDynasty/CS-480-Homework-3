fe40641f57ca87694ef35d116d9cbca08a0d7b2b
TurtleDynasty <TurtleDynasty@users.noreply.github.com>
2016-03-23 17:16:09 -0700

    starting code

---
+++ oshw3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+#define BLOCK_SIZE 8
+
+int main ( int argc, char *argv[] ) {
+	if (argc != 3){
+		printf("Please two arguments, an infile and an outfile.\n");
+	}
+	//inputs and outputs numerical references with read and write privileges, respectively
+	int infile = open(argv[1], O_RDONLY);
+	int outfile = open(argv[2], O_WRONLY);
+
+
+	return 0;
+}
