d8df336f1e585d0fda5e97c2a7a9ceb7385dc83a
TurtleDynasty <TurtleDynasty@users.noreply.github.com>
2016-03-23 17:22:16 -0700

    A complete program that does nothing
    Checks for param umber, opens and closes files

--- oshw3.c
+++ oshw3.c
@@ -6,11 +6,13 @@
 int main ( int argc, char *argv[] ) {
 	if (argc != 3){
 		printf("Please two arguments, an infile and an outfile.\n");
+		return 0;
 	}
 	//inputs and outputs numerical references with read and write privileges, respectively
 	int infile = open(argv[1], O_RDONLY);
 	int outfile = open(argv[2], O_WRONLY);
 
-
+	close(infile);
+	close(outfile);
 	return 0;
 }
