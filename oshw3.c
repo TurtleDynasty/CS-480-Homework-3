77e1f244ed4026007026171a09879d89c95b1de0
TurtleDynasty <TurtleDynasty@users.noreply.github.com>
2016-03-23 17:47:31 -0700

    Add stats feature
    number of bytes in infile can be read

--- oshw3.c
+++ oshw3.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <fcntl.h>
 
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 #define BLOCK_SIZE 8
 
 int main ( int argc, char *argv[] ) {
@@ -11,6 +15,11 @@ int main ( int argc, char *argv[] ) {
 	//inputs and outputs numerical references with read and write privileges, respectively
 	int infile = open(argv[1], O_RDONLY);
 	int outfile = open(argv[2], O_WRONLY);
+
+	struct stat buff;
+	stat(argv[1], &buff);
+	int num_bytes = buff.st_size;
+	printf("number of bytes in infile = %i\n", num_bytes);
 
 	close(infile);
 	close(outfile);
